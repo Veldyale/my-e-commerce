f4f0faef775a91f816b73bb35a957ac8ed335af2
veldyaea <veldyale@icloud.com>
2021-09-13 15:20:29 +0400

    not yet

--- my_ecommerce/mainapp/models.py
+++ my_ecommerce/mainapp/models.py
@@ -5,16 +5,6 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
-#************
-#1 Category
-# Product
-# Cartproduct
-#4 Cart
-#5 Order
-#************
-#6 Customer
-#7 Specification
-
 
 class Category(models.Model):
 
@@ -54,6 +44,36 @@ class Product(models.Model):
         return self.title
 
 
+class Notebook(Product):
+
+    diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    processor_freq = models.CharField(max_length=255, verbose_name='Частота процессора')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
+    video = models.CharField(max_length=255, verbose_name='Видеокарта')
+    time_without_charge = models.CharField(max_length=255, verbose_name='Время работы аккумулятора')
+
+    def __str__(self):
+        return (f'{self.category.name} : {self.title}')
+
+
+class Smartphone(Product):
+
+    diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    resolution = models.CharField(max_length=255, verbose_name='Разрешение экрана')
+    accum_volume = models.CharField(max_length=255, verbose_name='Объем батареи')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
+    sd = models.BooleanField(default=True)
+    sd_volume_max = models.CharField(max_length=255, verbose_name='Максимальный объем встраиваемой памяти')
+    main_cam_mp = models.CharField(max_length=255, verbose_name='Основная камера')
+    frontal_cam_mp = models.CharField(max_length=255, verbose_name='Фронтальная камера_')
+
+
+    def __str__(self):
+        return (f'{self.category.name} : {self.title}')
+
+
 class CartProduct(models.Model):
 
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
@@ -86,11 +106,3 @@ class Customer(models.Model):
     def __str__(self):
         return (f'Покупатель: {self.user.first_name} {self.user.last_name}')
 
-# class Specifications(models.Model):
-#
-#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-#     object_id = models.PositiveIntegerField()
-#     name = models.CharField(max_length=255, verbose_name='Наименование товара для хараетеристик')
-#
-#     def __str__(self):
-#         return (f'Характеристики товара: {self.name}')
