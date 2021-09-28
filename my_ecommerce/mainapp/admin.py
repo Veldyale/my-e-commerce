a45b660780a93fdeb779e8c6d6fa4642bb8b4032
veldyaea <veldyale@icloud.com>
2021-09-28 08:47:47 +0400

    17 compleated

--- my_ecommerce/mainapp/admin.py
+++ my_ecommerce/mainapp/admin.py
@@ -4,6 +4,21 @@ from django.utils.safestring import mark_safe
 from .models import *
 
 
+class SmartphoneAdminForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        instance = kwargs.get('instance')
+        if not instance.sd:
+            self.fields['sd_volume_max'].widget.attrs.update({
+                'readonly': True, 'style': 'background: lightgray;'
+            })
+
+    def clean(self):
+        if not self.cleaned_data['sd']:
+            self.cleaned_data['sd_volume_max'] = None
+        return self.cleaned_data
+
 class NotebookAdminForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
@@ -25,6 +40,9 @@ class NotebookAdmin(admin.ModelAdmin):
 
 
 class SmartphoneAdmin(admin.ModelAdmin):
+
+    change_form_template = 'admin.html'
+    form = SmartphoneAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "category":
--- my_ecommerce/mainapp/templatetags/specifications.py
+++ my_ecommerce/mainapp/templatetags/specifications.py
@@ -1,5 +1,6 @@
 ﻿from django import template
 from django.utils.safestring import mark_safe
+from mainapp.models import Smartphone
 
 register = template.Library()
 
@@ -54,4 +55,11 @@ def get_product_spec(product, model_name):
 @register.filter
 def product_spec(product):
     model_name = product.__class__._meta.model_name
+    if isinstance(product, Smartphone):
+        if not product.sd:
+            PRODUCT_SPEC['smartphone'].pop('Максимальный объем карты памяти:')
+            # PRODUCT_SPEC['smartphone']['Поддержка карт памяти:'] = '1'
+        else:
+            # PRODUCT_SPEC['smartphone']['Поддержка карт памяти:'] = '2'
+            PRODUCT_SPEC['smartphone']['Максимальный объем карты памяти:'] = 'sd_volume_max'
     return mark_safe(TABLE_HEAD + get_product_spec(product, model_name) + TABLE_TAIL)
